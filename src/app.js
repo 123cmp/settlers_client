ae4d7123b919fda13356f93c37b41f7a862d57a6
ApoZteriori <2080adnd2a>
2016-02-21 01:24:39 +0200

    trying create prototypr

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
-$('.cards .card').draggable({
-    revert: 'invalid'
+var someGame = angular.module('someGame',[]);
+
+someGame.controller('boardController', function ($scope) {
+    $scope.raceNumber = [1, 2, 3, 4];
+    $scope.showTableRace1 = function() {
+        $scope.tableRace1Class = 'table-race1-show';
+    };
+});
+someGame.factory('gameData', function(){
+
 });
 
-$('.row').droppable({
-    activeClass: 'drop-active',
-    hoverClass: 'drop-hover',
-
-    accept: '.card',
-    drop: function( event, ui ) {
-        $(ui.helper)
-            .removeAttr('style')
-            .draggable( 'disable' )
-            .removeClass('ui-draggable ui-draggable-handle')
-            .appendTo($(this));
-        
-    }
-});
+//$('.cards .card').draggable({
+//    revert: 'invalid'
+//});
+//
+//$('.row').droppable({
+//    activeClass: 'drop-active',
+//    hoverClass: 'drop-hover',
+//
+//    accept: '.card',
+//    drop: function( event, ui ) {
+//        $(ui.helper)
+//            .removeAttr('style')
+//            .draggable( 'disable' )
+//            .removeClass('ui-draggable ui-draggable-handle')
+//            .appendTo($(this));
+//
+//    }
+//});
