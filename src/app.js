39a1122f8d7a7fdbfaca04c7aa08500ec4feaf86
ApoZteriori <2080adnd2a>
2016-02-24 00:04:19 +0200

    work accordeon

--- src/app.js
+++ src/app.js
@@ -3,16 +3,15 @@ var someGame = angular.module('someGame',[]);
 someGame.controller('boardController', function ($scope) {
     $scope.raceNumber = [1, 2, 3, 4];
     $scope.showTableRace = function(index) {
-        console.info('.table-race' + index);
-        //siblings work uncorrect
-        //accordeon not work successful
+
         $scope.clickedRace =  $('.race' + index);
         $scope.clickedRace.toggleClass('active-race-bar');
         $scope.clickedRace.siblings().removeClass('active-race-bar');
 
         $scope.clickedTable = $('.table-race' + index);
         $scope.clickedTable.toggleClass('table-show-race'+index);
-        $scope.clickedTable.siblings().removeClass("table-show-race1", "table-show-race2", "table-show-race3" ,"table-show-race4");
+        $scope.clickedTable.siblings().removeClass("table-show-race1 table-show-race2 table-show-race3 table-show-race4");
+
     };
 });
 someGame.factory('gameData', function(){
