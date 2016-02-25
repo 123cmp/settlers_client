01727c5298db79268fc6734a26ed40912a3435e2
ApoZteriori <2080adnd2a>
2016-02-25 21:54:30 +0200

    rewrite logic (without jquery)

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
 var someGame = angular.module('someGame',[]);
 
 someGame.controller('boardController', function ($scope) {
-    $scope.raceNumber = [1, 2, 3, 4];
-    $scope.showTableRace = function(index) {
+    $scope.playerActiveBar = 3;
 
-        $scope.clickedRace =  $('.race' + index);
-        $scope.clickedRace.toggleClass('active-race-bar');
-        $scope.clickedRace.siblings().removeClass('active-race-bar');
-
-        $scope.clickedTable = $('.table-race' + index);
-        $scope.clickedTable.toggleClass('table-show-race'+index);
-        $scope.clickedTable.siblings().removeClass("table-show-race1 table-show-race2 table-show-race3 table-show-race4");
-
-    };
+    //$scope.showTableRace = function(index) {
+    //
+    //    //$scope.clickedRace =  $('.race' + index);
+    //    //$scope.clickedRace.toggleClass('active-race-bar');
+    //    //$scope.clickedRace.siblings().removeClass('active-race-bar');
+    //    //
+    //    //$scope.clickedTable = $('.table-race' + index);
+    //    //$scope.clickedTable.toggleClass('table-show-race'+index);
+    //    //$scope.clickedTable.siblings().removeClass("table-show-race1 table-show-race2 table-show-race3 table-show-race4");
+    //
+    //};
 });
 someGame.factory('gameData', function(){
 
 });
-
 
 //$('.cards .card').draggable({
 //    revert: 'invalid'
